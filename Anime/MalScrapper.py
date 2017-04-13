3a9096c68bb3b2bf663679a8e6de0044511c765a
Armand Syahtama <Armand.Syahtama@gmail.com>
2017-04-13 10:18:05 -0400

    "Completed AniList Link Maker Function"

--- Anime/MalScrapper.py
+++ Anime/MalScrapper.py
@@ -19,12 +19,12 @@ def get_anime_links(title):
         res = requests.get(search_url)
         res.raise_for_status()
         print(res)
-    except requests.exceptions.ConnectionError:
+    except requests.exceptions.RequestException:
         print("No such link {} found, exiting program".format(search_url))
         sys.exit(1)
 
     soup = bs4.BeautifulSoup(res.text, "html.parser")
-    links = [element.get("href") for element in soup.select("a.hoverinfo_trigger.fw-b.fl-l", limit=5)]
+    links = [element.get("href") for element in soup.select("a.hoverinfo_trigger.fw-b.fl-l", limit=3)]
     print(links)
 
     return links
@@ -39,16 +39,12 @@ def get_anime_info(link):
     try:
         res = requests.get(link)
         res.raise_for_status()
-    except requests.exceptions.ConnectionError:
+    except requests.exceptions.RequestException:
         print("Failed to connect to url")
-        return
-    except requests.exceptions.HTTPError:
-        print("Too many requests at once")
         return
 
     soup = bs4.BeautifulSoup(res.text, "html.parser")
-    synopsis = soup.find(itemprop='description').get_text()
-    print(synopsis)
+    synopsis = " ".join(soup.find(itemprop='description').get_text().split(" "))
 
     anime_info_dict = {'Name': soup.select("h1.h1")[0].text,
                        'Type': soup.select("div > a")[15].text,
@@ -57,30 +53,63 @@ def get_anime_info(link):
                                                                                          'Finished Airing'])))[0].strip(),
                        'Aired': " ".join(soup.select("div.spaceit")[1].text.strip().split(" ")[2:]),
                        'Source': " ".join(soup.select("div.spaceit")[3].text.split() if ("Source:" in soup.select(
-                           "div.spaceit")[3].text) else soup.select("div.spaceit")[4].text.split()),
+                           "div.spaceit")[3].text) else soup.select("div.spaceit")[4].text.split()).split(" ")[1],
                        'Genres': soup.select("div")[soup.select("div").index(soup.select("div.spaceit")[3] if (
                            "Source:" in
                            soup.select
-                           ("div.spaceit")[3].text) else soup.select("div.spaceit")[4]) + 1].text.strip().split(
-                           "\n")[1],
-                       'Duration': [int(s) for s in (soup.select("div.spaceit")[4].text if ("Duration:" in soup.select(
-                           "div.spaceit")[
-                           4].text) else soup.select("div.spaceit")[5].text).split() if s.isdigit()][0]}
-
-    print(anime_info_dict['Name'])
-    print(anime_info_dict['Type'])
-    print(anime_info_dict['Episodes'])
-    print(anime_info_dict['Status'])
-    print(anime_info_dict['Aired'])
-    print(anime_info_dict['Source'])
-    print(anime_info_dict['Genres'])
-    print(anime_info_dict['Duration'])
+                           ("div.spaceit")[3].text) else soup.select("div.spaceit")[4]) + 1].text.strip().split("\n")[1],
+                       'Duration': " ".join(soup.select("div.spaceit")[4].text.strip().split(" ")[2:] if
+                                            ("Duration:" in soup.select("div.spaceit")[4].text) else soup.select("div.spaceit")[
+                                                                                                         5].text.strip().split(" ")[2:])}
 
     return anime_info_dict['Name'], synopsis, anime_info_dict, link
 
 
+def anilist_link_maker(title):
+    """
+    Takes a title parameter and finds the anilist page for the specific anime
+    :param title: Full title of anime to search for
+    :return: An AniList link to the anime
+    """
+
+    title = "%20".join(title.split(" "))
+    client_info = {'grant_type': 'client_credentials',
+                   'client_id': 'my-id',
+                   'client_secret': 'my-secret'}
+
+    # Make a POST Request to anilist, returning back an access token for the GET requests
+    try:
+        post_anilist = requests.post('https://anilist.co/api/auth/access_token', data=client_info)
+        access_data = post_anilist.json()
+    except requests.exceptions.RequestException:
+        print("Failed to make the post request, returning")
+        return
+
+    # Make a GET Request to anilist, to get info on specific anime show
+    try:
+        anilist_url = 'https://anilist.co/api/anime/search/{0}?access_token={1}'.format(title, access_data['access_token'])
+        get_anilist_anime = requests.get(anilist_url)
+        show_info = get_anilist_anime.json()[0]
+    except requests.exceptions.RequestException:
+        print("Failed to make Get Request")
+        return
+
+    # Construct a link to the anime's anilist page, and test to see if it works before returning it
+    try:
+        anilist_anime_page = 'https://anilist.co/anime/{0}/{1}'.format(show_info['id'], "".join(show_info['title_romaji'].split(" ")))
+        test_link = requests.get(anilist_anime_page)
+        test_link.raise_for_status()
+        return anilist_anime_page
+    except requests.exceptions.RequestException:
+        print("Failed to make the last Request")
+        return
+
+
 def main():
-    # Retrieving title of anime
+    """
+    Main to be used for testing out this specific module
+    :return: 
+    """
     while True:
         print("What show are you looking up: ")
         title = str(input()).split(" ")
@@ -89,8 +118,19 @@ def main():
         anime_links = get_anime_links(title)
 
         for index, link in enumerate(anime_links):
-            print("Checking Link #{}".format(index))
-            get_anime_info(link)
+            print("\nChecking Link #{}".format(index + 1))
+            anime_name, anime_synopsis, anime_info_dict, link = get_anime_info(link)
+            print("Name: {}".format(anime_name))
+            print("Synopsis: {}".format(anime_synopsis))
+
+            for key, value in anime_info_dict.items():
+                print("{0}: {1}".format(key, value))
+
+            print("\nNow just wait...")
+            sleep(5)
+            anilist_page = anilist_link_maker(anime_name)
+            print("Anilist Link: {}".format(anilist_page))
+            print("\nWait again...")
             sleep(5)
 
 
