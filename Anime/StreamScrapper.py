352b7a9caf8b4592f7bd64c704972f697369e5a2
Armand Syahtama <Armand.Syahtama@gmail.com>
2017-04-21 21:19:21 -0400

    "Added Funimation Search"

--- Anime/StreamScrapper.py
+++ Anime/StreamScrapper.py
@@ -1,3 +1,6 @@
+import string
+import requests
+
 from crunchyroll.apis.meta import MetaApi
 
 
@@ -9,9 +12,27 @@ def search_crunchyroll(anime):
     return crunchyroll_listing[0].url
 
 
+def search_funimation(anime):
+    """Checks if anime exists on Funimation website"""
+    exclude = set(string.punctuation)
+    anime = ''.join(ch for ch in anime if ch not in exclude)
+    anime = '-'.join(anime.split(" ")).lower()
+    funi_url = f'https://www.funimation.com/shows/{anime}/'
+
+    try:
+        funi_test = requests.get(funi_url)
+        funi_test.raise_for_status()
+    except requests.exceptions.HTTPError:
+        print('Got a 404 error, looks like this wasn\'t a valid link')
+        return
+
+    return funi_url
+
+
 def main():
     print(search_crunchyroll('Cowboy Bebop'))
     print(search_crunchyroll('Sword Art Online'))
+    print(search_funimation('Cowboy Bebop'))
 
 
 if __name__ == '__main__':
