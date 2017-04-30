7f9e4dc063b41a0af5c484dd3d5e0c6e5ce158ab
Armand Syahtama <Armand.Syahtama@gmail.com>
2017-04-30 16:47:13 -0400

    "Added function in AniDBScraper that makes URL to AniDB page"

--- Anime/AniDBScrapper.py
+++ Anime/AniDBScrapper.py
@@ -1,13 +1,9 @@
 import requests
-import pprint
 import os
 
 
 def scrape_anidb():
-    """
-    Open up anidb dat file, containing anime titles and id's and writting them to a text file
-    :return: 
-    """
+    """Open up anidb dat file, containing anime titles and id's and writting them to a text file"""
 
     if os.stat('AniDBTitles.txt').st_size > 0:
         print('No need to run this script again, let\'s limit the amount of requests')
@@ -26,18 +22,33 @@ def scrape_anidb():
         ani.write(anidb_request.text)
 
 
-def search_titles():
+def _get_animeid(title):
     """Searches through AniDB Titles"""
     with open('AniDBTitles.txt', 'r', encoding='utf8') as ani:
         anidb_titles = ani.read()
         anidb_titles = anidb_titles.split("\n")
         anidb_titles = [t for t in anidb_titles if "|en|" in t]
-    pprint.pprint(anidb_titles)
+    english_anime_dict = {}
+    for anime in anidb_titles:
+        anime = anime.split("|")
+        english_anime_dict[anime[3]] = anime[0]
+    return english_anime_dict[title]
+
+
+def get_anidb_link(title):
+    """Takes an anime title, and makes a link to the associated AniDB page"""
+    try:
+        animeid = _get_animeid(title)
+    except KeyError:
+        print('Either title was mispelled or does not exist')
+        return
+    return f'https://anidb.net/perl-bin/animedb.pl?show=anime&aid={animeid}'
 
 
 def main():
     scrape_anidb()
-    search_titles()
+    print(get_anidb_link('Nanbaka (2017)'))
+    print(get_anidb_link('Attack on Titan'))
 
 
 if __name__ == '__main__':
