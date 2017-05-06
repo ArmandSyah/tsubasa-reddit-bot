02c2e0cf131b33152c24e9d91107243768916e79
Armand Syahtama <Armand.Syahtama@gmail.com>
2017-05-06 10:15:12 -0400

    "Added another successful unittest"

--- testing/animemoduletests.py
+++ testing/animemoduletests.py
@@ -8,11 +8,19 @@ class TestMalScrapper(unittest.TestCase):
     def setUp(self):
         pass
 
-    def mal_scrapper_test(self):
+    def test_get_links(self):
         title = 'Renai Boukun'
         mal_links = malscrapper.get_anime_links(title)
         pprint.pprint(mal_links)
         self.assertIsNotNone(mal_links)
+
+    def test_scrape_info(self):
+        title = 'Jojo'
+        mal_links = malscrapper.get_anime_links(title)
+        jojo_anime = malscrapper.MALAnimeInfo(mal_links[0])
+        pprint.pprint(jojo_anime.get_names())
+        self.assertIsNotNone(jojo_anime)
+
 
 if __name__ == '__main__':
     unittest.main()
