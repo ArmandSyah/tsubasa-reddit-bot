7167d82f5e82ff8066443468bda31ad99fa59764
Armand Syahtama <Armand.Syahtama@gmail.com>
2017-05-10 19:21:13 -0400

    "Added mal search by using mal api"

--- anime/malinfo/malsearch.py
+++ anime/malinfo/malsearch.py
@@ -23,40 +23,50 @@ def get_links_by_google_search(title):
 
 
 def get_links_by_spice(title):
+    """Use Spice_API MAL Wrapper to retrive anime_id and use it to construct MAL link"""
     mal_config = config.load_mal_config()
     try:
         mal_credentials = spice.init_auth(mal_config['mal_username'], mal_config['mal_password'])
         mal_search = spice.search(title.strip(), spice.get_medium('anime'), mal_credentials)
-    except ValueError:
+    except:
         return
     anime_id = mal_search[0].id
-    print(anime_id)
-    return f"https://myanimelist.net/anime/{anime_id}"
+    mal_url = f"https://myanimelist.net/anime/{anime_id}"
+    return mal_url
+
+
+def get_links_by_mal_api(title):
+    """Use MAL's Official API to retrieve anime_id and use it to construct a MAL link"""
+    mal_config = config.load_mal_config()
+    try:
+        mal_api_search = f"https://myanimelist.net/api/anime/search.xml?q={title.strip()}"
+        mal_credentials = (mal_config['mal_username'], mal_config['mal_password'])
+        mal_request = make_get_request(mal_api_search, mal_credentials)
+        mal_soup = BeautifulSoup(mal_request.text, 'lxml')
+        mal_entries = mal_soup.anime
+        anime_listings = [anime for anime in mal_entries.findAll('entry')]
+        anime_id = anime_listings[0].id.get_text()
+        mal_url = f"https://myanimelist.net/anime/{anime_id}"
+    except:
+        return
+    return mal_url
 
 
 def get_links_by_brute_force(title):
-    """
-    Uses beautiful soup to enter anime into search bar and then extract links of anime pages from the search results
-    :type title: str
-    :return: Mal Search Links
-    """
+    """Enter anime into MAL search bar and scrape for the first available MAL anime link"""
 
     title = "%20".join(title.split(" "))
     mal_search_url = f"https://myanimelist.net/anime.php?q={title}"
-    try:
-        res = requests.get(mal_search_url)
-        res.raise_for_status()
-    except requests.exceptions.HTTPError:
-        print("Might have made too many requests here")
-        return
-    soup = BeautifulSoup(res.text, "html.parser")
+    mal_request = make_get_request(mal_search_url)
+    soup = BeautifulSoup(mal_request.text, "html.parser")
     links = [element.get("href") for element in soup.select("a.hoverinfo_trigger.fw-b.fl-l", limit=5)]
-    return links
+    mal_url = links[0]
+    return mal_url
 
 
-def make_get_request(url):
+def make_get_request(url, credentials=None):
     try:
-        r = requests.get(url)
+        r = requests.get(url, auth=credentials)
         r.raise_for_status()
     except requests.exceptions.HTTPError:
         return
@@ -64,7 +74,7 @@ def make_get_request(url):
 
 
 def main():
-    print(get_links_by_spice('naruto'))
+    print(get_links_by_mal_api('naruto'))
 
 if __name__ == '__main__':
     os.chdir('\\'.join(os.getcwd().split('\\')[:-1]))
