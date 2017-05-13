afb906c7cf460c2974440c13ebd02667dcb5d9be
Armand Syahtama <Armand.Syahtama@gmail.com>
2017-05-13 09:36:26 -0400

    Ensure only one instance of BeautifulSoup is made per anime

--- anime/mal/malanime.py
+++ anime/mal/malanime.py
@@ -1,10 +1,11 @@
+from anime import utilities
 from . import malsoup
 
 
 class MalAnime(object):
     def __init__(self, url):
         """Computation of fields only done when necessary"""
-        self._url = url
+        self._soup = utilities.make_beatiful_soup(url)
         self._id = pull_mal_id(url)
         self._synopsis = None
         self._main_name = None
@@ -26,73 +27,73 @@ class MalAnime(object):
     @property
     def synopsis(self):
         if self._synopsis is None:
-            self._synopsis = malsoup.scrape_synopsis(self._url)
+            self._synopsis = malsoup.scrape_synopsis(self._soup)
         return self._synopsis
 
     @property
     def main_name(self):
         if self._main_name is None:
-            self._main_name = malsoup.scrape_main_name(self._url)
+            self._main_name = malsoup.scrape_main_name(self._soup)
         return self._main_name
 
     @property
     def english_name(self):
         if self._english_name is None:
-            self._english_name = malsoup.scrape_english_name(self._url)
+            self._english_name = malsoup.scrape_english_name(self._soup)
         return self._english_name
 
     @property
     def japanese_name(self):
         if self._japanese_name is None:
-            self._japanese_name = malsoup.scrape_japanese_name(self._url)
+            self._japanese_name = malsoup.scrape_japanese_name(self._soup)
         return self._japanese_name
 
     @property
     def synonyms(self):
         if self._synonyms is None:
-            self._synonyms = malsoup.scrape_synonyms(self._url)
+            self._synonyms = malsoup.scrape_synonyms(self._soup)
         return self._synonyms
 
     @property
     def anime_type(self):
         if self._anime_type is None:
-            self._anime_type = malsoup.scrape_anime_type(self._url)
+            self._anime_type = malsoup.scrape_anime_type(self._soup)
         return self._anime_type
 
     @property
     def episodes(self):
         if self._episodes is None:
-            self._episodes = malsoup.scrape_episodes(self._url)
+            self._episodes = malsoup.scrape_episodes(self._soup)
         return self._episodes
 
     @property
     def status(self):
         if self._status is None:
-            self._status = malsoup.scrape_status(self._url)
+            self._status = malsoup.scrape_status(self._soup)
         return self._status
 
     @property
     def airdate(self):
         if self._airdate is None:
-            self._airdate = malsoup.scrape_airdate(self._url)
+            self._airdate = malsoup.scrape_airdate(self._soup)
         return self._airdate
 
     @property
     def source(self):
         if self._source is None:
-            self._source = malsoup.scrape_source(self._url)
+            self._source = malsoup.scrape_source(self._soup)
         return self._source
 
     @property
     def genres(self):
         if self._genres is None:
-            self._genres = malsoup.scrape_genres(self._url)
+            self._genres = malsoup.scrape_genres(self._soup)
         return self._genres
 
     @property
     def duration(self):
         if self._duration is None:
-            self._duration = malsoup.scrape_duration(self._url)
+            self._duration = malsoup.scrape_duration(self._soup)
         return self._duration
 
 
--- anime/mal/malsoup.py
+++ anime/mal/malsoup.py
@@ -1,40 +1,33 @@
 import re
 
-from anime import utilities
 
-
-def scrape_synopsis(mal_url):
-    soup = utilities.make_beatiful_soup(mal_url)
+def scrape_synopsis(soup):
     scraped_synopsis = soup.find(itemprop='description').get_text()
     formatted_synopsis = scraped_synopsis.strip()
     return formatted_synopsis
 
 
-def scrape_main_name(mal_url):
-    soup = utilities.make_beatiful_soup(mal_url)
+def scrape_main_name(soup):
     main_name = soup.select("h1.h1")[0].text
     formatted_main_name = main_name.strip()
     return formatted_main_name
 
 
-def scrape_english_name(mal_url):
-    soup = utilities.make_beatiful_soup(mal_url)
+def scrape_english_name(soup):
     english_name = soup.find_all(text=re.compile(r'^English.*'))[0]
     formatted_english_name = (" ".join(english_name.string.parent.parent.text.strip().split(" ")[1:])
                               if len(english_name) > 0 else '')
     return formatted_english_name
 
 
-def scrape_japanese_name(mal_url):
-    soup = utilities.make_beatiful_soup(mal_url)
+def scrape_japanese_name(soup):
     japanese_name = soup.find_all(text=re.compile(r'^Japanese.*'))[0]
     formatted_japanese_name = (" ".join(japanese_name.string.parent.parent.text.strip().split(" ")[1:])
                                if len(japanese_name) > 0 else '')
     return formatted_japanese_name
 
 
-def scrape_synonyms(mal_url):
-    soup = utilities.make_beatiful_soup(mal_url)
+def scrape_synonyms(soup):
     synonyms = (soup.find_all(text=re.compile(r'^Synonyms.*'))[0]
                 if len(soup.find_all(text=re.compile(r'^Synonyms.*'))) > 0 else list())
     formatted_synonyms = (" ".join(synonyms.string.parent.parent.text.strip().split(" ")[1:])
@@ -42,21 +35,18 @@ def scrape_synonyms(mal_url):
     return formatted_synonyms
 
 
-def scrape_anime_type(mal_url):
-    soup = utilities.make_beatiful_soup(mal_url)
+def scrape_anime_type(soup):
     anime_type = soup.select("div > a")[15].text
     return anime_type
 
 
-def scrape_episodes(mal_url):
-    soup = utilities.make_beatiful_soup(mal_url)
+def scrape_episodes(soup):
     episodes = soup.select("div.spaceit")[0].text
     formatted_episodes = episodes.split()[1]
     return formatted_episodes
 
 
-def scrape_status(mal_url):
-    soup = utilities.make_beatiful_soup(mal_url)
+def scrape_status(soup):
     status = soup.find_all(text=re.compile(r'\b(?:%s)\b' % '|'.join(['Currently Airing',
                                                                      'Finished Airing',
                                                                      'Not yet aired'])))[0]
@@ -64,15 +54,13 @@ def scrape_status(mal_url):
     return formatted_status
 
 
-def scrape_airdate(mal_url):
-    soup = utilities.make_beatiful_soup(mal_url)
+def scrape_airdate(soup):
     airdate = soup.select("div.spaceit")[1].text
     formatted_airdate = " ".join(airdate.strip().split(" ")[2:])
     return formatted_airdate
 
 
-def scrape_source(mal_url):
-    soup = utilities.make_beatiful_soup(mal_url)
+def scrape_source(soup):
     source = (soup.select("div.spaceit")[3].text.split()
               if ("Source:" in soup.select("div.spaceit")[3].text)
               else soup.select("div.spaceit")[4].text.split())
@@ -80,8 +68,7 @@ def scrape_source(mal_url):
     return formatted_source
 
 
-def scrape_genres(mal_url):
-    soup = utilities.make_beatiful_soup(mal_url)
+def scrape_genres(soup):
     genres = soup.select("div")[soup.select("div").index(soup.select("div.spaceit")[3]
                                                          if ("Source:" in soup.select("div.spaceit")[3].text)
                                                          else soup.select("div.spaceit")[4]) + 1]
@@ -90,8 +77,7 @@ def scrape_genres(mal_url):
     return formatted_genres
 
 
-def scrape_duration(mal_url):
-    soup = utilities.make_beatiful_soup(mal_url)
+def scrape_duration(soup):
     duration = (soup.select("div.spaceit")[4].text.strip().split(" ")[2:]
                 if ("Duration:" in soup.select("div.spaceit")[4].text)
                 else soup.select("div.spaceit")[5].text.strip().split(" ")[2:])
