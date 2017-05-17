d0d2f09b66829b7877bd7b449ec698f0a42d3c12
Armand Syahtama <Armand.Syahtama@gmail.com>
2017-05-16 22:04:02 -0400

    Cleaned up mal description of anime

--- tests/basictests.py
+++ tests/basictests.py
@@ -6,14 +6,7 @@ from anime.anidb import anidbsearchmethods as anidbsearch
 def mal_object_test():
     jojo_mal = malsearchmethods._get_mal_links_by_google_search('Hinako Note')
     jojo = MalAnime(jojo_mal)
-    print(f"Main Name: {jojo.main_name}")
-    print(f"English Name: {jojo.english_name}")
-    print(f"Japanese Name: {jojo.japanese_name}")
-    print(f"Synonyms: {jojo.synonyms}")
     print(f"Synopsis: {jojo.synopsis}")
-    print(f"Genres: {jojo.genres}")
-    print(f"Airdate: {jojo.airdate}")
-    print(f"Rating: {jojo.rating}/10")
 
 
 def anidb_link_test():
