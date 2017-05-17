3107096b274a3de49e1ecef4b598a155708560be
Armand Syahtama <Armand.Syahtama@gmail.com>
2017-05-17 08:00:59 -0400

    Continued with reddit markedup message

--- reddit/message.py
+++ reddit/message.py
@@ -7,8 +7,9 @@ from anime.streams import streamsearchmethods
 
 def make_message(title):
     """Constructs message to be sent to reddit user"""
-    comment = ""
     anime_info = _set_up(title)
+    comment = _construct_comment(anime_info)
+    print(comment)
 
 
 def _set_up(title):
@@ -29,3 +30,25 @@ def _set_up(title):
                          'anime': anime}
 
     return comment_info_dict
+
+
+def _construct_comment(anime_info):
+    comment = []
+    anime = anime_info['anime']
+    comment.append(f'# {anime.main_name} \n')
+    comment.append('***** \n')
+    comment.append(f'**Show Information: **\n')
+    comment.append(f'* [Anilist]({anime_info["anilist_url"]}) \n') \
+        if anime_info["anilist_url"] is not None else comment
+    comment.append(f'* [MyAnimeList]({anime_info["mal_url"]}) \n') \
+        if anime_info["mal_url"] is not None else comment
+    comment.append(f'* [AniDB]({anime_info["anidb_url"]}) \n') \
+        if anime_info["anidb_url"] is not None else comment
+    return ''.join(comment)
+
+
+def main():
+    print(make_message('Hinako Note'))
+
+if __name__ == '__main__':
+    main()
